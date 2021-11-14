f1b436177681203a1fa024ce4235e781c23c2c9e
Filip Zynwala <filipzynwala@gmail.com>
2021-11-14 19:22:05 +0100

    test

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -9,7 +9,7 @@ const App = () => {
             <ExampleComponent
                 text={toUpperCase('Create React Library Example 😄')}
             />
-            <ExampleComponent text={logGreeting('Jadwiga')} />
+            <ExampleComponent text={logGreeting('Jadwigaa')} />
         </>
     );
 };

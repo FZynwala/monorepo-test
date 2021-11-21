cc7e29c747be5c979b46e6c88fa25b01791c4e69
Filip Zynwala <filipzynwala@gmail.com>
2021-11-21 12:51:44 +0100

    fix

--- packages/frontend/ci/set-frontend-pipeline.sh
+++ packages/frontend/ci/set-frontend-pipeline.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-fly -t tutorial set-pipeline -p frontend-pipeline -c frontend-pipeline.yml
+fly -t tutorial set-pipeline -p frontend-pipeline -c frontend-pipeline.yml --load-vars-from vars.yml
